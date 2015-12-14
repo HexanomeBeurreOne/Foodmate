b11ea3393c2d1e1e30f401303b0d69acec283ebc
QuentinBay <quentin.bayart45430@gmail.com>
2015-12-14 10:54:48 +0100

    Display JSON contact in friends view

--- www/js/controllers/friends.js
+++ www/js/controllers/friends.js
@@ -3,6 +3,18 @@
 
 angular.module('FriendsController', [])
 
-    .controller('FriendsCtrl', function ($scope, $rootScope) {
-      $scope.test= "Foodmate friend";
+    .controller('FriendsCtrl', function ($scope, $rootScope, $http) {
+      	$scope.test= "Foodmate friend";
+
+
+	    $http.get('data/contacts.json')
+			.success(function(data) {
+				$scope.contacts = data.contacts;
+			})
+			.error(function(err) {
+				alert("Failed reading contacts.json")
+			});
+
+		
+
     });
