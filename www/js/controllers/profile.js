8a41664d9f7767d9301cad90529d87cc5f5b016b
MrBerts <berthier.gllm@gmail.com>
2015-12-14 17:31:22 +0100

    Modif profil, mise en place des onglets de preference

--- www/js/controllers/profile.js
+++ www/js/controllers/profile.js
@@ -3,6 +3,16 @@
 
 angular.module('ProfileController', [])
 
-    .controller('ProfileCtrl', function ($scope, $rootScope) {
-      $scope.viewName= "Profil";
-    });
+.controller('ProfileCtrl', function ($scope, $rootScope) {
+  $scope.viewName = "Profil";
+  $scope.prefRegime = true;
+  $scope.prefResto = false;
+  $scope.afficherRegime = function() {
+    $scope.prefRegime = true;
+    $scope.prefResto = false;
+  };
+  $scope.afficherResto = function() {
+    $scope.prefRegime = false;
+    $scope.prefResto = true;
+  };
+});
