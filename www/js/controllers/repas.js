46a99cbe024200fde3a9903210677d5e030e1772
V1C0D3R <V1C0D3R@gmail.com>
2015-12-15 11:18:19 +0100

    Fully mocked repas view

--- www/js/controllers/repas.js
+++ www/js/controllers/repas.js
@@ -3,6 +3,24 @@
 
 angular.module('RepasController', [])
 
-    .controller('RepasCtrl', function ($scope, $rootScope) {
+    .controller('RepasCtrl', function ($scope, $rootScope, $http) {
       $scope.viewName= "Repas";
+
+      // Creations du classement fictifs
+	    $http.get('data/chosenRestaurants.json')
+			.success(function(data) {
+				$scope.chosenRestaurants = data.chosenRestaurants;
+			})
+			.error(function(err) {
+				alert("Failed reading chosenRestaurants.json")
+			});
+
+		// Creations des amis fictifs du repas
+	    $http.get('data/mealFriends.json')
+			.success(function(data) {
+				$scope.mealFriends = data.mealFriends;
+			})
+			.error(function(err) {
+				alert("Failed reading mealFriends.json")
+			});
     });
