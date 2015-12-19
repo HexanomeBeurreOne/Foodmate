7bfe2aea8f5254347fcd2292691b7c000888047a
Adrien Menella <adrien.menella@gmail.com>
2015-12-19 01:05:29 +0100

    modif css vue choix-menus

--- www/js/controllers/choixMenus.js
+++ www/js/controllers/choixMenus.js
@@ -6,8 +6,6 @@ angular.module('ChoixMenusController', [])
     .controller('ChoixMenusCtrl', function ($scope, $rootScope, $http) {
       $scope.viewName= "Choix menus";
 
-      var restos;
-
 	// Génération des restos et menus
 	$http.get('data/restos.json')
 		.success(function(data) {
@@ -33,7 +31,7 @@ angular.module('ChoixMenusController', [])
 			 	var rowRestoId = "row-resto-"+compteurRestos;
 
 				if (compteurRestos%2==0) {
-					angular.element(document.getElementById('restos-container')).append("<div class='row responsive-sm' id="+rowRestoId+"><div class='col col-50'><div class='card'><div class='item item-text-wrap'><img src="+resto.picture+" alt="+resto.name+"></div><div class='item item-divider text-center' style=background-color:"+resto.color+">"+resto.name+"</div></div>");
+					angular.element(document.getElementById('restos-container')).append("<div class='row responsive-sm' id="+rowRestoId+"><div class='col col-50'><div class='card'><div class='item item-text-wrap background-center' style='background-image:url("+resto.picture+")'></div><div class='item item-divider text-center color-white' style=background-color:"+resto.color+">"+resto.name+"</div></div>");
 				} 
 				// si le compteur est IMPAIRE on ajoute seulement l'ensemble du html pour représenter une card de resto
 				else {
@@ -42,7 +40,7 @@ angular.module('ChoixMenusController', [])
 					var previousRowRestoId = "row-resto-"+lastRestoIndex;
 
 					// après avoir récupérè la dernière div.row (grâce à l'id recréé précédemment) on ajoute l'ensemble du html pour représenter une card de resto
-					angular.element(document.getElementById(previousRowRestoId)).append("<div class='col col-50'><div class='card'><div class='item item-text-wrap'><img src="+resto.picture+" alt="+resto.name+"></div><div class='item item-divider text-center' style=background-color:"+resto.color+">"+resto.name+"</div>");
+					angular.element(document.getElementById(previousRowRestoId)).append("<div class='col col-50'><div class='card'><div class='item item-text-wrap background-center' style='background-image:url("+resto.picture+")'></div><div class='item item-divider text-center color-white' style=background-color:"+resto.color+">"+resto.name+"</div>");
 				};
 
 				compteurRestos++;
@@ -63,7 +61,7 @@ angular.module('ChoixMenusController', [])
 
 			 		// si le compteur est PAIRE on ajoute une div avec la classe row ET l'ensemble du html pour représenter une card de menu
 			 		if (compteurMenus%2==0) {
-						angular.element(document.getElementById('menus-container')).append("<div class='row responsive-sm' id="+rowMenuId+"><div class='col col-50'><div class='card'><div class='item item-text-wrap'><span>"+menu.price+" €</span><img src="+menu.picture+" alt="+menu.name+"></div><div class='item item-divider text-center' style=background-color:"+resto.color+">"+menu.name+"</div></div>");
+						angular.element(document.getElementById('menus-container')).append("<div class='row responsive-sm' id="+rowMenuId+"><div class='col col-50'><div class='card'><div class='item item-text-wrap background-center' style='background-image:url("+resto.picture+")'><span class='price'>"+menu.price+" €</span></div><div class='item item-divider text-center color-white' style=background-color:"+resto.color+">"+menu.name+"</div></div>");
 					} 
 					// si le compteur est IMPAIRE on ajoute seulement l'ensemble du html pour représenter une card de menu
 					else {
@@ -72,7 +70,7 @@ angular.module('ChoixMenusController', [])
 						var previousRowMenuId = "row-menu-"+lastMenuIndex;
 
 						// après avoir récupérè la dernière div.row (grâce à l'id recréé précédemment) on ajoute l'ensemble du html pour représenter une card de menu
-						angular.element(document.getElementById(previousRowMenuId)).append("<div class='col col-50'><div class='card'><div class='item item-text-wrap'><span>"+menu.price+" €</span><img src="+menu.picture+" alt="+menu.name+"></div><div class='item item-divider text-center' style=background-color:"+resto.color+">"+menu.name+"</div>");
+						angular.element(document.getElementById(previousRowMenuId)).append("<div class='col col-50'><div class='card'><div class='item item-text-wrap background-center' style='background-image:url("+resto.picture+")'><span class='price'>"+menu.price+" €</span></div><div class='item item-divider text-center color-white' style=background-color:"+resto.color+">"+menu.name+"</div>");
 					};
 
 					// on incrémente le compteur de menus
@@ -88,6 +86,7 @@ angular.module('ChoixMenusController', [])
 			angular.element(document.getElementById('menus-container')).append("<h2>Une erreur est survenue</h2>");
 		});
 
-			
+		
+		
 
-    });
+ });
