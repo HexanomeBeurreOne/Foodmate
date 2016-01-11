e189eb36c0e518e4fb485a8937303b959a532425
Adrien Menella <adrien.menella@gmail.com>
2016-01-11 23:56:37 +0100

    save settings in profile

--- www/js/controllers/profileRestau.js
+++ www/js/controllers/profileRestau.js
@@ -6,7 +6,23 @@ angular.module('ProfileRestauController', [])
 .controller('ProfileRestauCtrl', function ($scope, $rootScope, $http, $state) {
   $scope.viewName = "Restaurants préférés";
 
-  $scope.dataRegimes = $rootScope.profile.regimes;
+  //$scope.dataRegimes = $rootScope.profile.regimes;
   $scope.dataRestos = $rootScope.profile.restaurants;
+
+  $scope.saveSettings = function(){
+  	$state.go("app.profile");
+  };
+
+  $scope.toggleChecked = function(parentIndex,index) {
+  	console.log(index, $rootScope.profile.restaurants);
+  	if ($rootScope.profile.restaurants[parentIndex].nom[index].checked === false) {
+  		$rootScope.profile.restaurants[parentIndex].nom[index].checked = true;
+  	}
+  	else {
+  		$rootScope.profile.restaurants[parentIndex].nom[index].checked = false;
+  	};
+  };
+
+
 
 });
